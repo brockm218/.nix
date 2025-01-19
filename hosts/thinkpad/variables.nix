81e326e1a80da363bead0958f6a81503fb0c5b93
installer <installer@gmail.com>
2025-01-19 01:33:28 -0600

    Initial laptop config commit.

---
+++ hosts/thinkpad/variables.nix
@@ -0,0 +1,16 @@
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "Brock McCrory";
+  gitEmail = "brockm218@gmail.com";
+
+  # Hyprland Settings
+  extraMonitorSettings = "monitor=,5120x1440@240,auto,1";
+
+  # Waybar Settings
+  clock24h = false;
+
+  # Program Options
+  browser = "zen"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "ghostty"; # Set Default System Terminal
+  keyboardLayout = "us";
+}
